0ed9ea92a20302cd05999500dc234c06795e518d
David0073 <velenciamartinezjosedavid@gmail.com>
2026-08-27 10:51:23 -0500

    Primer avance del proyecto

---
+++ evaluador.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class evaluador {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int cantidadProducto;
+        int productosCategoriaAlta = 0;
+        int indice;
+
+       final double LIMITE_CATEGORIA_BAJA = 10000;
+       final double LIMITE_CATEGORIA_MEDIA = 50000;
+
+        
+    }
+}
+
