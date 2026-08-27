27c97eacda71d3874f56da83748eca896a75ba27
David0073 <velenciamartinezjosedavid@gmail.com>
2026-08-27 18:27:46 -0500

    segudo avance del proyecto

--- evaluador.java
+++ evaluador.java
@@ -1,16 +1,17 @@
 import java.util.Scanner;
 
-public class evaluador {
+public class Evaluador {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int cantidadProducto;
-        int productosCategoriaAlta = 0;
-        int indice;
 
-       final double LIMITE_CATEGORIA_BAJA = 10000;
-       final double LIMITE_CATEGORIA_MEDIA = 50000;
+        int cantidadProductos;
+        int productosCategoriaAlta = 0;
+
+        final double LIMITE_CATEGORIA_BAJA = 10000;
+        final double LIMITE_CATEGORIA_MEDIA = 50000;
+
+        System.out.print("¿Cuántos productos desea evaluar? ");
+        cantidadProductos = scanner.nextInt();
 
         
-    }
 }
-
