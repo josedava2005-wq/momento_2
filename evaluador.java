e7bb897cd981690a52dd3911a4e686946b5d0aa3
David0073 <velenciamartinezjosedavid@gmail.com>
2026-08-27 18:40:47 -0500

    Completa el evaluador

--- evaluador.java
+++ evaluador.java
@@ -1,17 +1,37 @@
-import java.util.Scanner;
-
-public class Evaluador {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        int cantidadProductos;
-        int productosCategoriaAlta = 0;
-
-        final double LIMITE_CATEGORIA_BAJA = 10000;
-        final double LIMITE_CATEGORIA_MEDIA = 50000;
-
-        System.out.print("¿Cuántos productos desea evaluar? ");
-        cantidadProductos = scanner.nextInt();
-
-        
-}
+import java.util.Scanner; 
+ 
+public class Evaluador { 
+    public static void main(String[] args) { 
+        Scanner scanner = new Scanner(System.in); 
+ 
+        int cantidadProductos; 
+        int productosCategoriaAlta = 0; 
+ 
+        final double LIMITE_CATEGORIA_BAJA = 10000; 
+        final double LIMITE_CATEGORIA_MEDIA = 50000; 
+ 
+        System.out.print("¿Cuántos productos desea evaluar? "); 
+        cantidadProductos = scanner.nextInt(); 
+ 
+        for (int indice = 1; indice <= cantidadProductos; indice++) { 
+            System.out.print("Digite el precio del producto " + indice + ": "); 
+            double precio = scanner.nextDouble(); 
+ 
+            if (precio <= LIMITE_CATEGORIA_BAJA) { 
+                System.out.println("Categoría baja"); 
+            } else if (precio <= LIMITE_CATEGORIA_MEDIA) { 
+                System.out.println("Categoría media"); 
+            } else { 
+                System.out.println("Categoría alta"); 
+                productosCategoriaAlta++; 
+            } 
+        } 
+ 
+        System.out.println( 
+            "Cantidad de productos de categoría alta: " 
+            + productosCategoriaAlta 
+        ); 
+ 
+        scanner.close(); 
+    } 
+} 
